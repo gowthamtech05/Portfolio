be1313b987b1f8a98d5bb1bc6704530302549ded
Gowtham <gowthamtech05@gmail.com>
2026-04-19 21:49:37 +0530

    search-engine

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1>Hello Gowtham</h1>;
+  return (
+    <h1 style={{ color: "black", textAlign: "center", marginTop: "50px" }}>
+      WORKING
+    </h1>
+  );
 }
 
 export default App;
