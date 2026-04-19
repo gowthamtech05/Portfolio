dc83863a54eb359754b868d802653cbfccb2cf8a
Gowtham <gowthamtech05@gmail.com>
2026-04-19 22:07:56 +0530

    search-engine

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,44 @@
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "./context/ThemeContext";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import Education from "./components/Education";
+
 function App() {
   return (
-    <h1 style={{ color: "black", textAlign: "center", marginTop: "50px" }}>
-      WORKING
-    </h1>
+    <ThemeProvider>
+      <BrowserRouter>
+        <Navbar />
+        <main>
+          <section id="hero">
+            <Hero />
+          </section>
+
+          <section id="about" className="scroll-mt-16">
+            <About />
+          </section>
+
+          <section id="skills">
+            <Skills />
+          </section>
+          <section id="education" className="scroll-mt-16">
+            <Education />
+          </section>
+
+          <section id="projects">
+            <Projects />
+          </section>
+
+          <section id="contact">
+            <Contact />
+          </section>
+        </main>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
